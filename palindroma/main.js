dfb86167c73b9dd89d3ddf4e867260c621e30677
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-01 17:30:55 +0100

    messo fuori il log degli array

--- palindroma/main.js
+++ palindroma/main.js
@@ -9,15 +9,20 @@ const parolaReverse = [];
 
 
 for (let i = 0; i < parolaEsame.length; i++) {
-    console.log(parola);
     parola.push(parolaEsame[i]);
 }
 
+console.log(parola);
+
+
 
 for (let i = parolaEsame.length - 1; i >= 0 ; i--) {
-    console.log(parolaReverse);
     parolaReverse.push(parolaEsame[i]);
 }
+
+console.log(parolaReverse);
+
+
 
 if (parola === parolaReverse) {
     console.log('yesss');
